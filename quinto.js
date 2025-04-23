3331bdeea1fd60264e4727ac0ee096a6ed767cd4
RogerAnthony2005 <rogerdenji4@gmail.com>
2025-04-23 11:56:35 -0400

    examen
    algo

---
+++ quinto.js
@@ -0,0 +1,21 @@
+//1. convertir la siguiente funcion a una funcion flecha  ennuna solo linea sin llaves(1p)
+function esPar(numero) {
+    if (numero % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+//2. Crear unha funcion callback que acepte un arreglo de numeros y una funcion callback donde duplique cada elemento del arreglo 
+//3. Usando los metodos setTimeOut y setInterval crear un contador que disminuya del 5 al 0 
+// Dado el siguiente arreglo:
+const libros = [
+    { id: 1, titulo: "Cien años de soledad", autor: "Gabriel García Márquez", año: 1967 },
+    { id: 2, titulo: "La casa de lo Espiritus", autor: "Isabel Allende", año: 1982 },
+    { id: 3, titulo: "Rayuela", autor: "Julio Cortázar", año: 1963 },
+    { id: 4, titulo: "El código Da Vinci", autor: "Dan Brown", año: 2005 }
+  ];
+
+//4. Cambiar el nombre del libro con id 3 a Final del juego usando for..of
+//5. Crear una  funcion que muestre solo los titulos de arreglo libros usando for..of
